46ffd0d95b53a8ead5cd515fb5df369c12103654
Tom Seifert <syex@seifertion.de>
2021-02-26 16:15:52 +0100

    Display list of puppies

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,23 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.model.Puppy
+import com.example.androiddevchallenge.model.puppyList
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -36,11 +48,71 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// Start building your app here!
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        PuppyList(puppies = puppyList)
+    }
+}
+
+@Composable
+fun PuppyList(puppies: List<Puppy>) {
+    LazyColumn(
+        contentPadding = PaddingValues(top = 16.dp)
+    ) {
+        items(items = puppies) { puppy ->
+            PuppyItem(puppy)
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Composable
+private fun PuppyItem(puppy: Puppy) {
+    Column(modifier = Modifier
+        .clip(RoundedCornerShape(4.dp))
+        .clickable { }
+        .padding(horizontal = 16.dp)) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Card(
+                modifier = Modifier
+                    .size(80.dp),
+                elevation = 2.dp
+            ) {
+                Image(
+                    painter = painterResource(puppy.pictureRes),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = puppy.nickname,
+                    style = MaterialTheme.typography.h6
+                )
+
+                Text(text = "${puppy.breed} - ${puppy.gender}")
+
+                Text(text = "Born: ${puppy.dateBorn}")
+            }
+        }
+
+        Text(
+            text = puppy.shortSummary,
+            modifier = Modifier.padding(
+                vertical = 8.dp
+            )
+        )
+
+        Divider()
     }
 }
 
